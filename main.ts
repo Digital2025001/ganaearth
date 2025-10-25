3ee9dcd6afcd809e6869368061f742388d30437f
Digital2025001 <bosmichael30@gmail.com>
2025-10-25 09:09:15 -0500

    Update redirect location in main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://sirendesigns.net/", // Replace this with your actual link
+      Location: "https://dcopv.com/irr/", // Replace this with your actual link
     },
   });
 });
