67db50971d51a29fdd25aae8e487dd06aeaddeb1
Digital2025001 <bosmichael30@gmail.com>
2025-11-05 13:16:22 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://", // Replace this with your actual link
+      Location: "https://hvdaconversions.com", // Replace this with your actual link
     },
   });
 });
