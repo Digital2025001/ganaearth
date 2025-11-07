6f5151d80d14d40b28758be10dd596a0ef69bdb1
Digital2025001 <bosmichael30@gmail.com>
2025-11-07 15:55:58 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://s.com", // Replace this with your actual link
+      Location: "http://TheNetMeds.com/", // Replace this with your actual link
     },
   });
 });
