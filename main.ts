a22ca2cbd8e3ca4ad20fe0ae43e244957b19e0fd
Digital2025001 <bosmichael30@gmail.com>
2025-11-12 14:53:05 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://hahaios.com/", // Replace this with your actual link
+      Location: "https://3la6ol.net/", // Replace this with your actual link
     },
   });
 });
