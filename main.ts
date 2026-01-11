4bb719a2e22875e6457a24076c9b25c9aae4c3eb
Digital2025001 <bosmichael30@gmail.com>
2026-01-11 13:31:39 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://peussasbz.vercel.app/", // Replace this with your actual link
+      Location: "https://5ymah.net/", // Replace this with your actual link
     },
   });
 });
